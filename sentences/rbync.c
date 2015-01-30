789126829ad353b8c529b9287212c33f64797af5
rbync <rbync@mail.missouri.edu>
2015-01-30 15:21:49 -0600

    create new c file named rbync.c

---
+++ sentences/rbync.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int rbync_first(){
+	printf("\n%s","So the Wizard made a decision.");
+	return 1;
+	}
+
+int rbync_second(){
+	printf("%s\n","The wizard was satisfied with what he had done and smiled.");
+	return 1;
+}
+
+
